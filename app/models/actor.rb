2e150b9fa392ddc1f2fdea814ae239f86d1805d3
shannonhall22 <shannonhall22@gmail.com>
2015-05-09 16:46:10 -0500

    added lists to actor page for roles and movies

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,9 @@
 class Actor < ActiveRecord::Base
+  def role
+    return Role.where(:actor_id => self.id)
+  end
+
+  def movies
+    return Movie.find(self.movie_id)
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,8 @@ class Movie < ActiveRecord::Base
   def director
     return Director.find(self.director_id)
   end
+
+  def role
+    return Role.where(:actor_id => self.id)
+  end
 end
