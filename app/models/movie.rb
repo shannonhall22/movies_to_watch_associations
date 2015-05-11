da2928023753e34ba252e8a51a41a7f582e14b5d
shannonhall22 <shannonhall22@gmail.com>
2015-05-11 12:15:16 -0500

    part 3 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,4 +6,16 @@ class Movie < ActiveRecord::Base
   def role
     return Role.where(:movie_id => self.id)
   end
+
+  def actors
+    actors = []
+    self.role.each do |role|
+      actors << role.actor
+    end
+    return actors
+  end
+
+  def actors_test
+    self.role.map { |role| role.actor }
+  end
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -6,4 +6,8 @@ class Role < ActiveRecord::Base
   def movie
     return Movie.find(self.movie_id)
   end
+
+  def actors
+    return Actor.find(self.actor_id)
+  end
 end
